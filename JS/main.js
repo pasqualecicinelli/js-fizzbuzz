9d7f298d0f04cc281ce9953f5b41772a8ef92ddd
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-21 16:03:53 +0200

    feat print in console

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,39 @@
 // ## PROCEDIMENTO:
-//
 // Creare un ciclo for che stampa in console i numeri da 1 a 100
-// -  fare un if
-//     -   per i numeri che sono sia multipli di "i" (contatore del ciclo) % 3 = 0 and "i" % 5 = 0 allora stampi "FizzBuzz"
-//     - altrimenti se è solo multipli di 3 stampi "Fizz"
-//     - altrimenti se è solo multipli di 5 stampi "Buzz"
-//
-//     altrimenti stampa solo i numeri
-//
+
+for (let i = 1; i <= 100; i++) {
+
+    // fare un if
+    // per i numeri che sono sia multipli di "i" (contatore del ciclo) % 3 = 0 and "i" % 5 = 0 allora stampi "FizzBuzz"
+
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+
+        let j = i;
+        console.log(j = 'FizzBuzz');
+
+        // altrimenti se è solo multipli di 3 stampi "Fizz"  
+
+    } else if (i % 3 == 0) {
+
+        console.log("Fizz");
+
+
+        // altrimenti se è solo multipli di 5 stampi "Buzz"
+
+    } else if (i % 5 == 0) {
+
+        console.log("Buzz");
+
+        // altrimenti stampa solo i numeri
+    } else {
+        console.log(i);
+    }
+
+}
+
+
+
+
 // ### BONUS 1:
 // Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
 // ### BONUS 2:
