522021392436e0144e5d6872c843c59a2a3b41e1
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-21 18:43:24 +0200

    add style

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,25 @@
 // ## PROCEDIMENTO:
 // Creare un ciclo for che stampa in console i numeri da 1 a 100
 
+const containerElement = document.getElementById("cont");
+
+
+
+
 for (let i = 1; i <= 100; i++) {
 
     // fare un if
     // per i numeri che sono sia multipli di "i" (contatore del ciclo) % 3 = 0 and "i" % 5 = 0 allora stampi "FizzBuzz"
 
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card')
+    let writeInside;
+
     if ((i % 3 == 0) && (i % 5 == 0)) {
 
-        let j = i;
-        console.log(j = 'FizzBuzz');
+        console.log('FizzBuzz');
+        writeInside = 'FizzBuzz';
+        cardElement.style.backgroundColor = "red";
 
         // altrimenti se è solo multipli di 3 stampi "Fizz"  
 
@@ -17,19 +27,32 @@ for (let i = 1; i <= 100; i++) {
 
         console.log("Fizz");
 
+        writeInside = ('Fizz');
+
+        cardElement.style.backgroundColor = "#0CD6A1";
+       
 
         // altrimenti se è solo multipli di 5 stampi "Buzz"
 
     } else if (i % 5 == 0) {
 
         console.log("Buzz");
-
+        writeInside = ('Buzz');
+        cardElement.style.backgroundColor = "#FFD166";
+        
+        
         // altrimenti stampa solo i numeri
     } else {
         console.log(i);
+        writeInside = (i);
+        cardElement.style.backgroundColor = "#1389B2";
+        
     }
 
+    cardElement.innerHTML = writeInside;
+    containerElement.append(cardElement);
 }
+
 
 
 
